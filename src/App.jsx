53fe64e20c8d23acdf17f8dbaaf3b783b4d1c15b
Livia Kaufman <k.livia@wustl.edu>
2025-11-16 00:19:28 -0600

    taking turns placing X or O in order

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,7 @@ import { useState } from 'react';
 
 function Square({ value, onSquareClick }) {
   return (
-    <button 
-      className="square" 
-      onClick={onSquareClick}
-    >
+    <button className="square" onClick={onSquareClick}>
         {value}
       </button>
   );
@@ -16,6 +13,9 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+    if (squares[i]) {
+      return;
+    }
     const nextSquares = squares.slice();
     if (xIsNext) {
       nextSquares[i] = 'X';

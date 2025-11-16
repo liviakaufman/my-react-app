b34fcc0bac0481a2d90ef850a4f458b0fd1d5438
Livia Kaufman <k.livia@wustl.edu>
2025-11-15 23:40:06 -0600

    make square display an X

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import {useState} from 'react';
 
 function Square() {
   const[value, setValue] = useState(null);
-  
+
   function handleClick() {
-    console.log('Clicked!');
+    setValue('X');
   }
   return (
   <button className="square" onClick={handleClick}>{value}</button>
@@ -15,19 +15,19 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value="1"/>
-        <Square value="2"/>
-        <Square value="3"/>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="4"/>
-        <Square value="5"/>
-        <Square value="6"/>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="7"/>
-        <Square value="8"/>
-        <Square value="9"/>
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );  

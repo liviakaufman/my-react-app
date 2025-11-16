e09637febc1a2edb0811b30d0f6a7cfdf0967467
Livia Kaufman <k.livia@wustl.edu>
2025-11-16 00:54:52 -0600

    display past moves, looking at history mapped

--- src/App.jsx
+++ src/App.jsx
@@ -62,13 +62,32 @@ export default function Game() {
     setXIsNext(!xIsNext);
   }
 
+  function jumpTo(nextMove) {
+    setXIsNext((nextMove % 2) === 0);
+    setHistory(history.slice(0, nextMove + 1));
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
   return (
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>{/* TODO */}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
